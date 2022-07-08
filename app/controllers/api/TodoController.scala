58e363715b5e13a1e53d4ca9a3c9a60cdf961d41
Alexander Alvarenga <moltron_nano@hotmail.com>
2022-07-07 21:19:27 -0600

    feat: add dummy route to retrieve by id

--- app/controllers/api/TodoController.scala
+++ app/controllers/api/TodoController.scala
@@ -5,6 +5,7 @@ import play.api.libs.json._
 import play.api.mvc._
 
 import javax.inject._
+import scala.concurrent.Future
 
 class TodoController @Inject()(
                                 cc: ControllerComponents
@@ -18,4 +19,15 @@ class TodoController @Inject()(
     val todosList = List(todo, todo2)
     Ok(Json.toJson(todosList))
   }
+
+  def getByIdRandom(id: Long) = Action {
+    //dbConfig.db.run(todoList.filter(_.id === id).delete)
+    val todo = new Todo(1, "item 1", false)
+    val todo2 = new Todo(2, "item 2", false)
+    val todosList = List(todo, todo2)
+    Ok(Json.toJson(todosList.filter(xtodo => xtodo.id == id)))
+
+  }
+
+
 }
